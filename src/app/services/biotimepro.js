6dc685e84572b448ac95cb2acb7c36111e3048bd
Anthony Tepach <anthony.tepach@gmail.com>
2026-05-04 16:24:39 -0600

    Refactor fetch functions to use buildApiUrl utility and enhance error messages

--- src/app/services/biotimepro.js
+++ src/app/services/biotimepro.js
@@ -1,22 +1,27 @@
+const buildApiUrl = (endpoint, queryParams) => {
+  const baseUrl = `${process.env.API_IP}:${process.env.API_PORT}`;
+  return new URL(`${baseUrl}/${endpoint}`, document.baseURI).href + '?' + new URLSearchParams(queryParams).toString();
+};
+
 export const fetchPunchTime = async (type, date_one, date_two) => {
-  const url = `http://192.168.200.3:3000/api/consulta?departamento=${type}&fecha_inicio=${date_one}&fecha_fin=${date_two}`;
-  
+  const url = buildApiUrl('api/consulta', { departamento: type, fecha_inicio: date_one, fecha_fin: date_two });
+
   const response = await fetch(url);
   if (response.ok) {
     const data = await response.json(); // Agregamos 'await' aquí para esperar la respuesta JSON.
     return data;
   } else {
-    throw new Error('Error fetching data');
+    throw new Error('Error fetching punch time data');
   }
 };
 
 export const fetchEmployeePunchTime = async (emp_code, date_one, date_two) => {
-  const url = `http://192.168.200.3:3000/api/employee/${emp_code}?fecha_inicio=${date_one}&fecha_fin=${date_two}`;
+  const url = buildApiUrl(`/api/employee/${emp_code}`, { fecha_inicio: date_one, fecha_fin: date_two });
 
   const response = await fetch(url);
   if (response.ok) {
     return response.json();
   } else {
-    throw new Error('Error fetching data');
+    throw new Error('Error fetching employee punch time data');
   }
 };
